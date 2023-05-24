a70de91c65d8da866cb909db6416bd8406f0c636
Joseph2812 <87885207+Joseph2812@users.noreply.github.com>
2023-05-24 22:56:22 +0100

    Change InteriorObjectExtended have (Obj[] & Pos[])

--- Scripts/Generation/InteriorObjectExtended.cs
+++ Scripts/Generation/InteriorObjectExtended.cs
@@ -1,4 +1,5 @@
 using Godot;
+using Godot.Collections;
 using System;
 using System.Collections.Generic;
 
@@ -10,27 +11,30 @@ public partial class InteriorObjectExtended : InteriorObject
     /// Other <c>InteriorObject</c>s that pairs with this <c>InteriorObject</c> (meant to generate together). 
     /// </summary>
     [Export]
-    private InteriorObject[] _extensions = null;
+    private Array<Array<InteriorObject>> _extensions = null;
 
     /// <summary>
     /// Where should all the <c>_extensions</c> be placed relative to this <c>InteriorObject</c>.
     /// </summary>
     [Export]
-    private Vector3[] _placementPositions = null;
+    private Array<Vector3[]> _placementPositions = null;
 
     public void CreateExtensionsRecursively(Vector3I originPos, float rotationY)
     {
-        foreach (Vector3 relativePos in _placementPositions)
+        for (int i = 0; i < _extensions.Count; i++)
         {
-            Vector3I nextPos = originPos + GetRotatedPosition(relativePos, rotationY);
+            for (int j = 0; j < _placementPositions.Count; j++)
+            {
+                Vector3I nextPos = originPos + GetRotatedPosition(_placementPositions[i][j], rotationY);
 
-            InteriorObject randomObj = _extensions[GridGenerator.Inst.Rng.RandiRange(0, _extensions.Length - 1)];
-            HashSet<Vector3I> clearancePositions = randomObj.GetClearancePositions(nextPos, rotationY);
+                InteriorObject randomObj = _extensions[i][GridGenerator.Inst.Rng.RandiRange(0, _extensions[i].Count - 1)];
+                HashSet<Vector3I> clearancePositions = randomObj.GetClearancePositions(nextPos, rotationY);
 
-            if (!GridGenerator.Inst.IsPlacementValid(clearancePositions)) { continue; }
-            GridGenerator.Inst.CreateInteriorNode(randomObj.Scene, nextPos, rotationY, clearancePositions);
+                if (!GridGenerator.Inst.IsPlacementValid(clearancePositions)) { continue; }
+                GridGenerator.Inst.CreateInteriorNode(randomObj.Scene, nextPos, rotationY, clearancePositions);
 
-            if (randomObj is InteriorObjectExtended extendedObj) { extendedObj.CreateExtensionsRecursively(nextPos, rotationY); }
+                if (randomObj is InteriorObjectExtended extendedObj) { extendedObj.CreateExtensionsRecursively(nextPos, rotationY); }
+            }
         }
     }
 }
